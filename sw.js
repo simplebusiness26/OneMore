013cadcd6c3290959d258b912b1ecc410851ac59
simplebusiness26 <simplebussiness26@gmail.com>
2026-08-24 04:23:18 +0100

    Cache gameplay control update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='one-more-v2';
-const ASSETS=['./','index.html','styles.css','game.js','manifest.webmanifest','icons/icon.svg'];
+const CACHE='one-more-v3';
+const ASSETS=['./','index.html','styles.css','game.js','gameplay-patch.js','manifest.webmanifest','icons/icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
